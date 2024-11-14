fe1ffa665473f852b572c69a9b17233ebb1401aa
Dyslex7c <polynsominiac@gmail.com>
2024-11-14 23:44:52 +0530

    chore: minor updates

--- Trees/treeTraversal.c
+++ Trees/treeTraversal.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Node {
+typedef struct Node
+{
     int data;
-    struct Node* left;
-    struct Node* right;
-};
+    struct Node *left;
+    struct Node *right;
+} Node;
 
-struct Node* createNode(int data) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    if (newNode == NULL) {
+Node *createNode(int data)
+{
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    if (newNode == NULL)
+    {
         printf("Memory allocation failed\n");
         exit(1);
     }
@@ -19,28 +22,32 @@ struct Node* createNode(int data) {
     return newNode;
 }
 
-struct Node* insertNode() {
+Node *insertNode()
+{
     int data;
     printf("Enter data (-1 for no node): ");
     scanf("%d", &data);
-    
-    if (data == -1) {
+
+    if (data == -1)
+    {
         return NULL;
     }
-    
-    struct Node* newNode = createNode(data);
-    
+
+    Node *newNode = createNode(data);
+
     printf("Enter left child of %d:\n", data);
     newNode->left = insertNode();
-    
+
     printf("Enter right child of %d:\n", data);
     newNode->right = insertNode();
-    
+
     return newNode;
 }
 
-void preorder(struct Node* root) {
-    if (root == NULL) {
+void preorder(Node *root)
+{
+    if (root == NULL)
+    {
         return;
     }
     printf("%d ", root->data);
@@ -48,8 +55,10 @@ void preorder(struct Node* root) {
     preorder(root->right);
 }
 
-void inorder(struct Node* root) {
-    if (root == NULL) {
+void inorder(Node *root)
+{
+    if (root == NULL)
+    {
         return;
     }
     inorder(root->left);
@@ -57,8 +66,10 @@ void inorder(struct Node* root) {
     inorder(root->right);
 }
 
-void postorder(struct Node* root) {
-    if (root == NULL) {
+void postorder(Node *root)
+{
+    if (root == NULL)
+    {
         return;
     }
     postorder(root->left);
@@ -66,8 +77,10 @@ void postorder(struct Node* root) {
     printf("%d ", root->data);
 }
 
-void freeTree(struct Node* root) {
-    if (root == NULL) {
+void freeTree(Node *root)
+{
+    if (root == NULL)
+    {
         return;
     }
     freeTree(root->left);
@@ -75,8 +88,30 @@ void freeTree(struct Node* root) {
     free(root);
 }
 
-void performTraversal(struct Node* root, int choice) {
-    switch (choice) {
+int main()
+{
+    Node *root = NULL;
+
+    printf("Create the binary tree:\n");
+    root = insertNode();
+
+    int choice;
+
+    do
+    {
+        printf("\nChoose traversal method:\n");
+        printf("1. Preorder Traversal\n");
+        printf("2. Inorder Traversal\n");
+        printf("3. Postorder Traversal\n");
+        printf("4. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        if (choice == 4)
+            break;
+
+        switch (choice)
+        {
         case 1:
             printf("Preorder traversal: ");
             preorder(root);
@@ -94,34 +129,10 @@ void performTraversal(struct Node* root, int choice) {
             break;
         default:
             printf("Invalid choice. Please try again.\n");
-    }
-}
-
-int main() {
-    struct Node* root = NULL;
-    
-    printf("Create the binary tree:\n");
-    root = insertNode();
-    
-    int choice;
-    
-    do {
-        printf("\nChoose traversal method:\n");
-        printf("1. Preorder Traversal\n");
-        printf("2. Inorder Traversal\n");
-        printf("3. Postorder Traversal\n");
-        printf("4. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-        
-        if (choice == 4) {
-            break;
         }
-        
-        performTraversal(root, choice);
-        
+
     } while (choice != 4);
-    
+
     freeTree(root);
     return 0;
 }
